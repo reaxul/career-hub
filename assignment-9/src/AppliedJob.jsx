18c93aa76ae10874fd73a1ece35956d510e34485
DevReaxul <hreazul11@gmail.com>
2023-04-12 21:05:35 +0600

    done

--- assignment-9/src/AppliedJob.jsx
+++ assignment-9/src/AppliedJob.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { appliedJobId } from "./utils/fakeDb";
 import { useLoaderData } from "react-router-dom";
 import PreviousApplied from "./Component/PreviousApplied";
@@ -7,11 +7,21 @@ const AppliedJob = () => {
   const appliedId = appliedJobId();
 
   const allJobs = useLoaderData();
+  const [job, setJobs] = useState(allJobs);
+  const remoteHandler = () => {
+    const remoteJob = allJobs.filter(job => job.remoteOrOnsite === 'Remote');
+    setJobs(remoteJob)
+  }
+  const onSiteHandler = () => {
+    const onsiteJob = allJobs.filter(job => job.remoteOrOnsite === 'Onsite');
+    setJobs(onsiteJob)
+  }
 
-  let carts = allJobs.filter((obj) => appliedId.includes(obj.id));
+  let carts = job.filter((obj) => appliedId.includes(obj.id));
 
   return (
     <div className="my-container">
+      <button onClick={remoteHandler} className="rounded h-8 text-white bg-gradient-to-r from-blue-500 to-purple-500 text-xs px-2 py-1 font-semibold">Remote Jobs</button> <button onClick={onSiteHandler}  className="rounded h-8 text-white bg-gradient-to-r from-blue-500 to-purple-500 text-xs px-2 py-1 font-semibold">Onsite Jobs</button>
       {carts.map((cart) => (
         <PreviousApplied key={cart.id} cart={cart}></PreviousApplied>
       ))}
--- assignment-9/src/Component/PreviousApplied.jsx
+++ assignment-9/src/Component/PreviousApplied.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const PreviousApplied = ({ cart }) => {
-    const navigate=useNavigate()
+  const navigate = useNavigate();
   const {
     companyLogo,
     jobTitle,
@@ -12,50 +12,54 @@ const PreviousApplied = ({ cart }) => {
     salary,
     fulltimeOrPartTime,
     id,
-  } = cart;
+    } = cart;
   return (
       <div className="my-4">
-          <div className=" flex border p-2 items-center justify-between">
-    
-    <div className="flex items-center">
-      <img
-        className="w-52 bg-gray-100 px-11 py-14 rounded-sm mr-3"
-        src={companyLogo}
-        alt=""
-      />
-      <div>
-        <h3 className="font-semibold text-xl">{jobTitle}</h3>
-        <p className="font-light text-gray-700 text-xs">{companyName}</p>
-        <p
-          className="inline-block border mr-2
+      <div className=" flex border p-2 items-center justify-between">
+        <div className="flex items-center">
+          <img
+            className="w-52 bg-gray-100 px-11 py-14 rounded-sm mr-3"
+            src={companyLogo}
+            alt=""
+          />
+          <div>
+            <h3 className="font-semibold text-xl">{jobTitle}</h3>
+            <p className="font-light text-gray-700 text-xs">{companyName}</p>
+            <p
+              className="inline-block border mr-2
                  px-2 border-purple-500 rounded text-xs text-purple-500 font-semibold items-center"
-        >
-          {remoteOrOnsite}
-        </p>
+            >
+              {remoteOrOnsite}
+            </p>
 
-        <p
-          className="inline-block border mb-4 mt-2
+            <p
+              className="inline-block border mb-4 mt-2
                  px-2 border-purple-500 rounded text-xs text-purple-500 font-semibold items-center"
-        >
-          {fulltimeOrPartTime}
-        </p>
-        <div className="flex gap-5 mb-3">
-          <div className="flex">
-            <img className="w-5 h-5" src="location.png" alt="" />
-            <small> {location}</small>
-          </div>
-          <div className="flex">
-            <img className="w-5 h-5" src="dollar.png" alt="" />
-            <small>{salary}</small>
+            >
+              {fulltimeOrPartTime}
+            </p>
+            <div className="flex gap-5 mb-3">
+              <div className="flex">
+                <img className="w-5 h-5" src="location.png" alt="" />
+                <small> {location}</small>
+              </div>
+              <div className="flex">
+                <img className="w-5 h-5" src="dollar.png" alt="" />
+                <small>{salary}</small>
+              </div>
+            </div>
           </div>
         </div>
+        <button
+          onClick={() => {
+            navigate(`/job-details/${id}`);
+          }}
+          className="rounded h-8 text-white bg-gradient-to-r from-blue-500 to-purple-500 text-xs px-2 py-1 font-semibold"
+        >
+          View Details
+        </button>
       </div>
     </div>
-    <button onClick={()=>{navigate(`/job-details/${id}`)}} className="rounded h-8 text-white bg-gradient-to-r from-blue-500 to-purple-500 text-xs px-2 py-1 font-semibold">
-      View Details
-    </button>
-  </div>
-      </div>
   );
 };
 
